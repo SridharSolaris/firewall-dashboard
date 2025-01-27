dc70adaf42aa691ad7deecfc8fb0c81464bce02c
SridharSolaris <21A91A05I2@aec.edu.in>
2025-01-27 09:37:20 +0530

    updated

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://firewall-backend.onrender.com"; // Backend API URL
+const API_BASE_URL = process.env.REACT_APP_FIREWALL_API; // Backend API URL
 
 // Helper to set Authorization headers
 const getAuthHeaders = (token) => ({
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:4000/api";
+const API_BASE_URL = process.env.REACT_APP_FIREWALL_API;
 
 // User sign up
 export const signup = async (userData) => {
